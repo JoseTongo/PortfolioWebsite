dad68a819bf917317ebb10467e09531444797340
JoseTongo01 <jose.tongo01@utrgv.edu>
2024-06-11 01:21:35 -0500

    Navigation

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,7 +36,23 @@ const MobileNav = () => {
             <CiMenuFries className="text-[32px] text-accent"/>
         </SheetTrigger>
         <SheetContent className="flex flex-col">
-            <div>logo</div>
+            {/* logo*/}
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                <h1>Jose Tongo<span className="text-accent">.</span></h1>
+                </Link>
+            </div>
+            {/*nav*/}
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link,index) => {
+                    return <Link 
+                    href={link.path}
+                     key={index}
+                    className={`capitalize font-medium hover:text-accent transition-all ${
+                    link.path === pathname ? "active-link" : ""
+                  }`}>{link.name}</Link>
+                })}
+            </nav>
         </SheetContent>
     </Sheet>
     ); 
